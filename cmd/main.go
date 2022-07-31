725b59f20d44f14157326ad0ce16bb56a2ce0c34
Ghibran Aljabbar <24712554+Ghibranalj@users.noreply.github.com>
2022-07-31 12:34:09 +0200

    feat: add basic cli funtionality (#10)
    * feat: add basic cli funtionality
    
    * feat: error when input file is empty

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	ksyaml "github.com/bukan-kambing/ks-yaml/pkg"
 )
 
+type opts struct {
+	inFile   string
+	toStdout bool
+	outFile  string
+	indent   int
+}
+
+var opt opts
+
 func main() {
-	b, err := ioutil.ReadFile("test.yml")
+	b, err := ioutil.ReadFile(opt.inFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	c := ksyaml.NewConverter()
+	c := ksyaml.NewConverter(ksyaml.WithIndentation(opt.indent))
 	ms := string(b)
 	os, _ := c.Convert(ms)
-	fmt.Println(os)
+
+	if opt.toStdout {
+		fmt.Println(os)
+		return
+	}
+
+	if err := ioutil.WriteFile(opt.outFile, []byte(os), 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	indent := flag.Int("i", 4, "indent")
+	outFile := flag.String("o", "", "output file")
+	inFile := flag.String("f", "", "input file")
+	flag.Parse()
+	opt.indent = *indent
+	opt.inFile = *inFile
+
+	if opt.inFile == "" {
+		fmt.Println("input file is required.")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *outFile != "" {
+		opt.outFile = *outFile
+	} else {
+		opt.toStdout = true
+	}
 }
